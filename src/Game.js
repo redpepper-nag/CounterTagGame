dc44a6fae5c5553b2cc3c057cc4eb1cab0c41d50
Louise Naginski <louisindiana@gmail.com>
2023-06-08 12:37:31 -0400

    game logic

--- src/Game.js
+++ src/Game.js
@@ -1,17 +1,36 @@
 import React from "react";
 import { TouchableOpacity, View, Text, StyleSheet } from 'react-native';
-import NfcManager, {NfcEvents} from "react-native-nfc-manager";
+import NfcManager, { NfcEvents } from "react-native-nfc-manager";
 
 function Game(props) {
-  async function scanTag() {
+  const [start, setStart] = React.useState(null);
+  const [duration, setDuration] = React.useState(0);
+
+  React.useEffect(() => {
+    let count = 5;
     NfcManager.setEventListener(NfcEvents.DiscoverTag, (tag) => {
-      console.warn('tag found', tag);
+      count--;
+      NfcManager.setAlertMessageIOS(`${count}...`);
+      if (count <= 0) {
+        NfcManager.unregisterTagEvent().catch(() => 0);
+        setDuration(new Date().getTime() - start.getTime());
+      }
     });
+
+    return () => {
+      NfcManager.setEventListener(NfcEvents.DiscoverTag, null);
+    }
+  }, [start]);
+
+  async function scanTag() {
     await NfcManager.registerTagEvent();
+    setStart(new Date());
+    setDuration(0);
   }
   return (
     <View style={styles.wrapper}>
       <Text>NFC Game</Text>
+      {duration > 0 && <Text>{duration} ms</Text>}
       <TouchableOpacity style={styles.button} onPress={scanTag}>
         <Text>START</Text>
       </TouchableOpacity>
